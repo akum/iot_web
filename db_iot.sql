995ea30422399ac37ccc79ff73cbb80469181376
Simon Monette <monette.simon@gmail.com>
2016-02-24 11:01:10 -0500

    SQL db now support thermal level

--- db_iot.sql
+++ db_iot.sql
@@ -11,11 +11,16 @@
 DROP TABLE IF EXISTS `tempLog`;
 CREATE TABLE `tempLog` (
   `timeStamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `Chambre` float NOT NULL,
-  `Chambre2` float NOT NULL,
-  `SalleBain` float NOT NULL,
-  `Salon` float NOT NULL,
+  `Chambre` decimal(5,2) NOT NULL,
+  `Chambre2` decimal(5,2) NOT NULL,
+  `SalleBain` decimal(5,2) NOT NULL,
+  `Salon` decimal(5,2) NOT NULL,
+  `HChambre` decimal(5,2) NOT NULL,
+  `HChambre2` decimal(5,2) NOT NULL,
+  `HSalleBain` decimal(5,2) NOT NULL,
+  `HSalon` decimal(5,2) NOT NULL,
+  `Exterieur` decimal(5,2) NOT NULL,
   PRIMARY KEY (`timeStamp`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dump completed on 2016-02-22 14:00:53
+-- Dump completed on 2016-02-22 16:45:22
