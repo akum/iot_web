f49e670276e26da6fc4de4ab0ac959851262c412
Simon Monette <monette.simon@gmail.com>
2016-02-24 11:04:40 -0500

    Track and add thermal info to DB

--- add.php
+++ add.php
@@ -8,8 +8,15 @@ if (!strcmp($secret, $key)){
 	$temp3 = $_POST["temp3"];
 	$temp4 = $_POST["temp4"];
 
-	$query = "INSERT INTO tempLog (Chambre, Chambre2, SalleBain, Salon)
-		VALUES ('".$temp1."','".$temp2."','".$temp3."','".$temp4."')";
+	$heat1 = $_POST["h1"];
+	$heat2 = $_POST["h2"];
+	$heat3 = $_POST["h3"];
+	$heat4 = $_POST["h4"];
+
+	$exter = $_POST["ex"];
+
+	$query = "INSERT INTO tempLog (Chambre, Chambre2, SalleBain, Salon, HChambre, HChambre2, HSalleBain, HSalon, Exterieur)
+		VALUES ('".$temp1."','".$temp2."','".$temp3."','".$temp4."','".$heat1."','".$heat2."','".$heat3."','".$heat4."','".$exter."')";
 
 	mysqli_query($link, $query);
 	mysqli_close($link);
