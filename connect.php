fa101e5a5aefc0a552a863804a7de0f2b1bd84ba
Simon Monette <monette.simon@gmail.com>
2016-02-24 14:21:13 -0500

    Removed white space

--- connect.php
+++ connect.php
@@ -2,17 +2,18 @@
 function db_connect(){
 	static $link;
 
-	if(!isset($link)) {
+	if(!isset($link)){
 		$config = parse_ini_file("config.ini");
 		$link = mysqli_connect($config['server'], $config['user'], $config['pass'], $config['db']);
 	}
 
-	if($link === false) {
+	if($link === false){
 		return mysqli_connect_error();
 	}
 
 	return $link;
 }
+
 function get_key(){
 	$config = parse_ini_file("config.ini");
 	return $config['secret'];
